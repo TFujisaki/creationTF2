25b302ecacf4691b26aad1b5f57aee392bfefc72
Tatsuya Fujisaki <taco@xf6.so-net.ne.jp>
2021-05-25 04:18:25 +0900

    新規追加　サイトmini さくらサーバー用ファイル

---
+++ サイトmini/JS/screen.js
@@ -0,0 +1,22 @@
+//画面サイズの取得
+getScreenSize();
+//ウィンドウサイズの取得
+getWindowSize();
+
+//画面サイズを取得する
+function getScreenSize() {
+	var s = "横幅 = " + window.parent.screen.width + " / 高さ = " + window.parent.screen.height;
+	document.getElementById("ScrSize").innerHTML = s;
+}
+
+//ウィンドウサイズを取得する
+function getWindowSize() {
+	var sW,sH,s;
+	sW = window.innerWidth;
+	sH = window.innerHeight;
+
+	s = "横幅 = " + sW + " / 高さ = " + sH;
+ 
+	document.getElementById("WinSize").innerHTML = s;
+}
+	
